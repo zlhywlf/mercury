8d2e0687b2f976f587272dbe2b4241d253ddda02
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2024-06-23 12:27:27 +0800

    chore: 抑制一些不必要的提示

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,6 +6,8 @@ Copyright (c) 2023-present 善假于PC也 (zlhywlf).
 import pytest
 from click.testing import CliRunner
 from mercury.__main__ import main
+
+# noinspection PyProtectedMember
 from mercury._version import version
 
 
@@ -18,6 +20,7 @@ from mercury._version import version
 )
 def test_main(cli_runner: CliRunner, command: str) -> None:
     """Test main."""
+    # noinspection PyTypeChecker
     result = cli_runner.invoke(main, [command])
     assert result.exit_code == 0
     assert version in result.output
